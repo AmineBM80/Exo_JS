d80a82c4165702359437e20fb7f0457225ccf896
BEN MANSOUR Amine <amine.s.benmansour@gmail.com>
2022-10-24 14:11:59 +0200

    correction EXO 5 evaluation 24/10

--- Assets/js/Evaluation_ex5 copy.js
+++ Assets/js/Evaluation_ex5 copy.js
@@ -18,24 +18,39 @@ let filtre6 = new RegExp ('^[a-zA-Z0-9.-_]+$');
 // let resultat_question = filtre6.test(document.form1.votre_question.value);
 
 let validation = document.getElementById("idSubForm3");
+//NOM//
 let nom = document.getElementById("nom");
 let nom_m = document.getElementById("nom_manquant");
+//PRENOM//
 let prenom = document.getElementById("prenom");
 let prenom_m = document.getElementById("prenom_manquant");
+//SEXE//
+let feminin = document.getElementById("F");
+let masculin = document.getElementById("M");
+let sexe_m = document.getElementById("sexe_manquant");
+//DATE//
+let date = document.getElementById("date");
+let date_m = document.getElementById("ddn_manquant");
+//CODE POSTAL//
 let cp = document.getElementById("cp");
 let cp_m = document.getElementById("cp_manquant");
+//ADRESSE//
 let adresse = document.getElementById("adresse");
 let adresse_m = document.getElementById("adresse_manquant");
+//VILLE//
 let ville = document.getElementById("ville");
 let ville_m = document.getElementById("ville_manquant");
+//MAIL//
 let email = document.getElementById("email_2");
 let email_m = document.getElementById("email_manquant");
+//QUESTION//
 let question = document.getElementById("votre_question");
 let question_m = document.getElementById("question_manquant");
 
 validation.addEventListener('click', valid);
 function valid(e)
 {
+    //NOM//
     if(nom.validity.valueMissing)
     {
         e.preventDefault();
@@ -48,7 +63,13 @@ function valid(e)
         nom_m.textContent = "Format incorrect"
         nom_m.style.color = "orange"
     }
+    else if(filtre1.test(nom.value) == true)
+    {
+        nom_m.textContent = "✓"
+        nom_m.style.color = "green"
+    }
 
+    //PRENOM//
     if(prenom.validity.valueMissing)
     {
         e.preventDefault();
@@ -61,7 +82,25 @@ function valid(e)
         prenom_m.textContent = "Format incorrect"
         prenom_m.style.color = "orange"
     }
+    else if(filtre1.test(prenom.value) == true)
+    {
+        prenom_m.textContent = "✓"
+        prenom_m.style.color = "green"
+    }
 
+    //SEXE//
+    if((feminin.checked != 1) && (masculin.checked != 1)){
+        e.preventDefault();
+        sexe_m.textContent = "Quel est votre sexe ?";
+        sexe_m.style.color = "red";
+    }
+    else if((feminin.checked = 1) || (masculin.checked = 1))
+    {
+        sexe_m.textContent = "✓"
+        sexe_m.style.color = "green"
+    }
+
+    //CODE POSTAL//
     if(cp.validity.valueMissing)
     {
         e.preventDefault();
@@ -74,7 +113,13 @@ function valid(e)
         cp_m.textContent = "Format incorrect"
         cp_m.style.color = "orange"
     }
+    else if(filtre2.test(cp.value) == true)
+    {
+        cp_m.textContent = "✓"
+        cp_m.style.color = "green"
+    }
 
+    //ADRESSE//
     if(adresse.validity.valueMissing)
     {
         e.preventDefault();
@@ -87,7 +132,13 @@ function valid(e)
         adresse_m.textContent = "Format incorrect"
         adresse_m.style.color = "orange"
     }
+    else if(filtre3.test(adresse.value) == true)
+    {
+        adresse_m.textContent = "✓"
+        adresse_m.style.color = "green"
+    }
 
+    //VILLE//
     if(ville.validity.valueMissing)
     {
         e.preventDefault();
@@ -100,7 +151,13 @@ function valid(e)
         ville_m.textContent = "Format incorrect"
         ville_m.style.color = "orange"
     }
+    else if(filtre4.test(ville.value) == true)
+    {
+        ville_m.textContent = "✓"
+        ville_m.style.color = "green"
+    }
     
+    //MAIL//
     if(email.validity.valueMissing)
     {
         e.preventDefault();
@@ -113,7 +170,13 @@ function valid(e)
         email_m.textContent = "Format incorrect"
         email_m.style.color = "orange"
     }
+    else if(filtre5.test(email.value) == true)
+    {
+        email_m.textContent = "✓"
+        email_m.style.color = "green"
+    }
 
+    //QUESTION//
     if(question.validity.valueMissing)
     {
         e.preventDefault();
@@ -126,9 +189,11 @@ function valid(e)
         question_m.textContent = "Format incorrect"
         question_m.style.color = "orange"
     }
-    else
+    else if(filtre6.test(question.value) == true)
     {
-
+        question_m.textContent = "✓"
+        question_m.style.color = "green"
     }
+    else{}
 }
 
--- Assets/js/JS08_ex5.js
+++ Assets/js/JS08_ex5.js
@@ -12,11 +12,17 @@ for (var i = 0; i <= string.length - 1; i++) {
     || string.charAt(i) == "e"
     || string.charAt(i) == "i"
     || string.charAt(i) == "o" 
-    || string.charAt(i) == "u") 
+    || string.charAt(i) == "u"
+    || string.charAt(i) == "y") 
     {
     voyelle += 1;
     }
 }
+let lemot = document.createElement('p')
+lemot.textContent = mot
+lemot.style.color = "red"
+document.body.appendChild(lemot);
+
 let nombrevoyelle = document.createElement('p');
 nombrevoyelle.textContent = "Le nombre de voyelle dans le mot est de : "+voyelle;
 document.body.appendChild(nombrevoyelle);
